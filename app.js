774a5fad7cb10b7a3d66c01e395f48257e5b1808
Sue Lucas <choi.soyoung@gmail.com>
2015-03-21 11:30:42 -0700

    updated files

---
+++ app.js
@@ -0,0 +1,64 @@
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
+    redis = require('redis'),
+    client = redis.createClient(),
+    nicknames = [];
+
+client.SMEMBERS("nickname", function(err, names){
+   nicknames = names;
+});
+
+server.listen(3000);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+var storeMessage = function(name, data){
+  var message = JSON.stringify({nick: name, msg: data});
+  client.lpush("new message", message, function(err, response){
+    client.ltrim("new message", 0, 9);
+  });
+};
+
+io.sockets.on('connection', function(socket){
+  socket.on('join', function(name, callback){
+    if (nicknames.indexOf(name) != -1){
+      callback(false);
+    } else {
+      callback(true);
+      socket.broadcast.emit("add chatter", name);
+      client.SMEMBERS('names', function(err, names){
+        names.forEach(function(name){
+          io.sockets.emit('add chatter', name);
+        });
+      });
+      
+      client.sadd("nickname", name);
+      socket.nickname = name;
+      updateNicknames();
+    }
+  });
+
+  function updateNicknames(){
+    client.SMEMBERS("nickname", function(err, names){
+        io.sockets.emit('usernames', names);
+    });
+  }
+
+  socket.on('send message', function(data){
+    io.sockets.emit('new message', {msg: data, nick: socket.nickname});
+  });
+
+  socket.on('disconnect', function(data){
+    if (!socket.nickname) return; //user leaves without entering username
+    socket.broadcast.emit("remove chatter", socket.nickname);
+    client.SREM("nickname", socket.nickname);
+    updateNicknames();
+  });
+
+});
